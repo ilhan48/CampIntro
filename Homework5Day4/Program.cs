aa39750bb77cfb2d32e2b4d0c427978be427fb3e
ilhan48 <58470523+ilhan48@users.noreply.github.com>
2021-01-29 04:32:09 +0300

    Homework5Day4

---
+++ Homework5Day4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Homework5Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class MyList<T> //  Generic class struct
+    {
+        T[] myArray;
+        T[] myTempArray;
+        public MyList() // constructor
+        {
+            myArray = new T[0]; // I reserved space for an array with 0 elements.
+
+        }
+        public void Add(T item) // Add method is born
+        {
+            myTempArray = myArray; // myTempArray holds reference number of myArray.
+            myArray = new T[myArray.Length + 1]; // Increment myArray element count by one.
+            for (int i = 0; i < myArray.Length; i++)   //The elements in the temporary array are written to the array using the for loop.
+            {
+                myArray[i] = myTempArray[i];
+            }
+        }
+
+    }
+}
